7ec859274f839aaa3289f090868818592b053dec
saori-eth <D84da@proton.me>
2024-01-30 00:48:23 -0600

    load glb

--- app/components/driver/index.ts
+++ app/components/driver/index.ts
@@ -1,5 +1,16 @@
-import { Engine, Scene, MeshBuilder, Vector3, HemisphericLight, FreeCamera, StandardMaterial, CubeTexture, Texture, Color3, PBRMaterial } from 'babylonjs';
-import { SkyMaterial } from 'babylonjs-materials';
+import {
+  Engine,
+  Scene,
+  MeshBuilder,
+  Vector3,
+  HemisphericLight,
+  FreeCamera,
+  SceneLoader,
+} from "babylonjs";
+import { SkyMaterial } from "babylonjs-materials";
+import "babylonjs-loaders";
+
+const BASE_URL = "https://saori-content.s3.amazonaws.com/";
 
 interface DriverProps {
   canvas: HTMLCanvasElement;
@@ -9,37 +20,54 @@ export class Driver {
   canvas: HTMLCanvasElement;
   engine: Engine;
   scene: Scene;
+  mesh: any;
   constructor(props: DriverProps) {
     this.canvas = props.canvas;
     this.engine = new Engine(this.canvas, true);
     this.scene = new Scene(this.engine);
+    this.mesh = {
+      // avatar: Mesh[]
+    };
     this.listeners();
     this.testScene();
+    this.loadGlb();
+  }
+
+  async loadGlb() {
+    SceneLoader.ImportMesh("", BASE_URL, "noun3d.glb", this.scene, (meshes) => {
+      meshes.forEach((mesh) => {
+        mesh.rotation = new Vector3(0, Math.PI, 0);
+        mesh.position.z = -7.5;
+        mesh.position.y = -0.25;
+      });
+      this.mesh.avatar = meshes;
+    });
   }
 
   listeners() {
-    window.addEventListener('resize', () => {
+    window.addEventListener("resize", () => {
       this.engine.resize();
     });
   }
 
   testScene() {
-    const camera = new FreeCamera('camera', new Vector3(0, 0, -10), this.scene);
+    const camera = new FreeCamera("camera", new Vector3(0, 0, -10), this.scene);
     camera.setTarget(Vector3.Zero());
 
-    const light = new HemisphericLight('light', new Vector3(0, 1, 0), this.scene);
+    const light = new HemisphericLight(
+      "light",
+      new Vector3(0, 1, 0),
+      this.scene
+    );
     light.intensity = 0.7;
 
-    const box = MeshBuilder.CreateBox('box', { size: 2 }, this.scene);
-
     // create skybox
-    const skybox = MeshBuilder.CreateBox('skybox', {size: 1000}, this.scene);
+    const skybox = MeshBuilder.CreateBox("skybox", { size: 1000 }, this.scene);
     const skyMaterial = new SkyMaterial("skyMaterial", this.scene);
     skyMaterial.backFaceCulling = false;
-    // Adjust these parameters for a night time or outer space look
-    skyMaterial.luminance = 1;  // Lower luminance for a darker sky
-    skyMaterial.turbidity = 2;    // Lower turbidity for less haze
-    skyMaterial.inclination = 0;  // Adjust the inclination for the position of the stars/moon
+    skyMaterial.luminance = 1;
+    skyMaterial.turbidity = 2;
+    skyMaterial.inclination = 0;
     skybox.material = skyMaterial;
 
     this.engine.runRenderLoop(() => {
