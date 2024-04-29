d053cbf49b899f5c88870176b146f830b80312bf
saori-eth <D84da@proton.me>
2024-04-28 21:22:35 -0500

    style uikit

--- components/GUI.tsx
+++ components/GUI.tsx
@@ -1,25 +1,30 @@
 import { Root, Text } from '@react-three/uikit'
+import { Card } from './Card'
 
 export const GUI = () => {
   return (
-    <group position={[0.75, 1.25, -0.25]} rotation={[0, -0.25, 0]}>
-      <Root
-        backgroundColor="black"
-        padding={24}
-        borderRadius={12}
-        maxWidth={375}
-      >
-        <Text color="white" fontSize={32}>
-          Hey I'm Saori!
-        </Text>
-        <Text color="white" fontSize={22} marginY={12}>
-          Foremost, I'm an onchain trader. In my downtime, I write TypeScript
-          and a bit of Solidity.
-        </Text>
-        <Text color="white" fontSize={22} marginY={12}>
-          I'm very interested in the future of 3d web, web3 and asset
-          interoperability via NFTs.
-        </Text>
+    <group position={[0.77, 1.25, -0.25]} rotation={[0, -0.25, 0]}>
+      <Root sizeX={0.75} sizeY={0.5}>
+        <Card
+          width={'100%'}
+          height={'100%'}
+          overflow={'scroll'}
+          padding={15}
+          alignItems={'flex-start'}
+          justifyContent={'space-evenly'}
+        >
+          <Text fontWeight={'bold'} fontSize={24}>
+            Hey, I'm Saori!
+          </Text>
+          <Text>
+            Foremost, I'm an onchain trader. In my downtime, I write TypeScript
+            and a bit of Solidity.
+          </Text>
+          <Text>
+            I'm very interested in the future of 3d web, web3 and asset
+            interoperability via NFTs.
+          </Text>
+        </Card>
       </Root>
     </group>
   )
