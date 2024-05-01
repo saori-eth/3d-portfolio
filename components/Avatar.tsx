2083182dba4cfb2aca14b705ca446270699483b1
saori-eth <D84da@proton.me>
2024-04-30 22:44:41 -0500

    add cursor

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -2,39 +2,13 @@ import { useClips } from '@/hooks/useClips'
 import { useVRMloader } from '@/hooks/useVRMLoader'
 import { SpotLight, useAnimations } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
-import { useEffect, useRef } from 'react'
-import { Object3D } from 'three'
+import { useEffect } from 'react'
 import { degToRad } from 'three/src/math/MathUtils.js'
 
 export const Avatar = () => {
   const vrm = useVRMloader('/avatar/avatar.vrm')
   const clips = useClips(vrm)
   const { mixer, actions } = useAnimations(clips, vrm.scene)
-  const lookAtTarget = useRef<Object3D>()
-  const defaultLookAtTarget = {
-    x: -0.717,
-    y: 0.502,
-  }
-
-  useEffect(() => {
-    const target = lookAtTarget.current
-    if (!target || !vrm.lookAt) return console.error('No lookAtTarget found')
-    target.position.set(defaultLookAtTarget.x, defaultLookAtTarget.y, 0)
-    vrm.lookAt.target = target
-    const onMouseMove = (e: MouseEvent) => {
-      target.position.x =
-        10.0 * ((e.clientX - 0.5 * window.innerWidth) / window.innerHeight)
-      target.position.y =
-        -10.0 * ((e.clientY - 0.5 * window.innerHeight) / window.innerHeight)
-    }
-    window.addEventListener('mousemove', onMouseMove)
-
-    const neckBone = vrm.humanoid.getNormalizedBoneNode('neck')
-    if (!neckBone) return console.error('No neck bone found')
-    neckBone.rotation.y = degToRad(90)
-
-    return () => window.removeEventListener('mousemove', onMouseMove)
-  }, [])
 
   useEffect(() => {
     if (!actions.idle) return console.error('No idle animation found')
@@ -49,8 +23,6 @@ export const Avatar = () => {
 
   return (
     <>
-      <primitive object={new Object3D()} ref={lookAtTarget} />
-
       <group position={[0.1, 0.5, 0]} rotation={[0, degToRad(45), 0]}>
         <primitive object={vrm.scene} />
       </group>
