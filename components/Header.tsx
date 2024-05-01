846abf073cda67becc1339d10ddea59611bb1ef3
saori-eth <D84da@proton.me>
2024-05-01 15:05:50 -0500

    remove header bg color

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import { TwitterLogo, GithubLogo } from '../app/assets/icons'
 
 export const Header = () => {
   return (
-    <div className="absolute top-0 left-0 w-full bg-black/50 z-10 flex items-center p-2 rounded-b-lg">
+    <div className="absolute top-0 left-0 w-full z-10 flex items-center p-2 rounded-b-lg">
       <Image
         src="/icon.png"
         alt="Saori Makishima"
@@ -11,12 +11,12 @@ export const Header = () => {
         height="30"
         className="mr-4 rounded-full"
       />
-      <div className="text-xl  text-white">Saori Makishima</div>
+      <div className="text-xl text-white">Saori Makishima</div>
       <a
         href="https://twitter.com/saori_xbt"
         target="_blank"
         rel="noopener noreferrer"
-        className="ml-auto p-2"
+        className="ml-auto p-2 pr-4"
       >
         <TwitterLogo color="white" size="24" />
       </a>
@@ -24,7 +24,7 @@ export const Header = () => {
         href="https://github.com/saori-eth"
         target="_blank"
         rel="noopener noreferrer"
-        className="p-2"
+        className="p-2 pr-6"
       >
         <GithubLogo color="white" size="24" />
       </a>
