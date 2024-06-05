9def3e4522a9f2aed99110a7837bb3152e0d29b6
saori-eth <D84da@proton.me>
2024-06-05 15:01:00 -0500

    fix loading bar outer width

--- components/LoadingScreen.tsx
+++ components/LoadingScreen.tsx
@@ -2,7 +2,7 @@ import { useProgress } from '@react-three/drei'
 import { useEffect, useState } from 'react'
 
 export const LoadingScreen = () => {
-  const { progress, item, loaded, total } = useProgress()
+  const { progress, item } = useProgress()
   const [items, setItems] = useState<string[]>([])
 
   useEffect(() => {
@@ -16,7 +16,7 @@ export const LoadingScreen = () => {
     <div className="fixed top-0 left-0 w-full h-full flex justify-start items-start bg-black text-white text-2xl">
       <div className="pl-8 pt-8 text-lime-500 font-mono">
         <div>Loading...</div>
-        <div className="w-full bg-gray-500">
+        <div className="w-full bg-gray-500" style={{ width: 300 }}>
           <div
             className="bg-lime-500 h-4"
             style={{ width: `${progress}%` }}
